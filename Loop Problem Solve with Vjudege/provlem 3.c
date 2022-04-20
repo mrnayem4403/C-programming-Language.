2175a116f15d4fc0a69a35395718de0307c5ee5f
Mostafizur Rahman Nayem <mrnayem4403@gmail.com>
2022-04-20 17:48:13 +0600

    Provlem 3 added

---
+++ Loop Problem Solve with Vjudege/provlem 3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int limit,months;
+    scanf("%d %d",&limit,&months);
+
+    int baki = 0;
+    for(int m=1; m<=months; m++ ) {
+        int used;
+        scanf("%d",&used);
+
+        int month_limit = limit + baki;
+        baki = month_limit-used;
+
+        printf("limit = %d, used = %d, baki = %d\n", month_limit,used,baki);
+    }
+    int final_limit = limit + baki;
+    printf("%d\n", final_limit);
+
+
+
+    return 0;
+}
