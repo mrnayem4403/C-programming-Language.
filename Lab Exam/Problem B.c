3fc0c1fda0b64e7d006d184a19a94223a57509e4
Mostafizur Rahman Nayem <mrnayem4403@gmail.com>
2022-04-22 12:29:47 +0600

    Lab Exan Problem B was solve

---
+++ Lab Exam/Problem B.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,count;
+    scanf("%d", &n);
+
+    for(count=1; count<=n; count++)
+    {
+        printf("%d Abracadabra\n",count);
+    }
+
+    return 0;
+}
